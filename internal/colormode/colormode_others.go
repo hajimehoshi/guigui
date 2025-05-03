7f2d337f72db1ef5cece1493a05b30adef76471d
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-05-04 00:35:00 +0300

    linux not working..

--- internal/colormode/colormode_others.go
+++ internal/colormode/colormode_others.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2025 The Guigui Authors
 
-//go:build !darwin && !linux && !windows
+//go:build !darwin && !windows
 
 package colormode
 
