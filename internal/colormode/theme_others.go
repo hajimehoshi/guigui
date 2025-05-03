654791d4bec863e3d2c878c41dba4148e0aa19f3
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-05-03 22:20:52 +0300

    theme_others.go fixed

--- internal/colormode/theme_others.go
+++ internal/colormode/theme_others.go
@@ -1,11 +1,10 @@
-//go:build !darwin && !windows
-
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2025 The Guigui Authors
+
+//go:build !darwin && !windows
 
 package colormode
 
 func systemColorMode() ColorMode {
-	// Unknown default to Light mode or what should I put in this function?
 	return Light
 }
