6d30acb62e19fa84c55023e1b738c6197332fa2f
realskyquest <135049479+realskyquest@users.noreply.github.com>
2025-09-14 05:55:33 +0500

    internal/clipboard: add fallback for linux

--- internal/clipboard/clipboard_js.go
+++ internal/clipboard/clipboard_js.go
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024 The Guigui Authors
 
+//go:build js
+
 package clipboard
 
 import "syscall/js"
--- internal/clipboard/clipboard_notjs.go
+++ internal/clipboard/clipboard_notjs.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024 The Guigui Authors
 
-//go:build !js
+//go:build !js && !linux && !freebsd
 
 package clipboard
 
